4a6c387a802fcaaca6aa81ae9f9fc3320196a5b4
Lautaro Epifani <epifanilautaro@gmail.com>
2024-04-17 11:29:09 +0200

    navigation screens

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,46 @@
+import Colors from "@/src/constants/Colors";
+import { StyleSheet, View, Text, Image, Pressable } from "react-native";
+import { Product } from "@/src/interfaces/interfaces";
+import { Link } from "expo-router";
+
+interface Props {
+  product: Product;
+}
+
+const ProductListItem = ({ product }: Props) => {
+    return (
+      <Link href={`/menu/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image source={{ uri: product.image || "defaultImage" }} style={styles.image} resizeMode="contain" />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+      </Link>
+    );
+  };
+ 
+  export default ProductListItem;
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'white',
+      padding: 10,
+      borderRadius: 20,
+      maxWidth: '50%',
+    },
+    image: {
+      width: "100%",
+      aspectRatio: 1,
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: "600",
+      marginVertical: 10,
+    },
+    price: {
+      color: Colors.light.tint,
+      fontWeight: "bold",
+    },
+  });
+  
